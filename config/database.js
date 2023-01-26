b61ee1310b73eca85aec43f812806b31d04a290b
c-garraway <c-garraway@users.noreply.github.com>
2023-01-26 10:59:42 -0500

    added date parser

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 "use strict"
 
 // db pool config
-const Pool = require('pg').Pool
+/* const Pool = require('pg').Pool */
+const pg = require('pg');
+const { Pool } = pg;
+
+pg.types.setTypeParser(1082, function(stringValue) {
+    return stringValue;  //1082 for date type
+  });
 
 const connectionString = process.env.CONNECTION_STRING
 
