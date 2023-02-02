0d81e26b69420c97f7d14a213b27ef6843c0bc96
c-garraway <c-garraway@users.noreply.github.com>
2023-02-02 15:05:22 -0500

    db ssl change

--- config/database.js
+++ config/database.js
@@ -13,7 +13,7 @@ const connectionString = process.env.CONNECTION_STRING
 
 const conObject = {
     connectionString,
-    ssl: { rejectUnauthorized: false }
+    //ssl: { rejectUnauthorized: false }
 }
 
 const pool = new Pool(conObject)
